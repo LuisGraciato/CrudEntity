79e03dc5cabe2a7b6a2cb0fec66e55fd95d7d3f0
LuisGraciato <luisgraciato@hotmail.com>
2023-03-03 16:22:15 -0300

    last

--- CrudRepository/Data/CadastroContext.cs
+++ CrudRepository/Data/CadastroContext.cs
@@ -1,16 +1,23 @@
 ﻿using CrudTreinoApi.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace CrudDomain.Data
 {
     public class CadastroContext : DbContext
     {
+        public CadastroContext()
+        {
+        }
         public CadastroContext(DbContextOptions<CadastroContext> opt) : base(opt)
         {
 
         }
         public DbSet<Cadastro> Cadastros { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+                    => optionsBuilder
+                    .UseSqlServer("Data Source=MAGNATI-10863-F\\SQLEXPRESS;Initial Catalog=EntityCrud;Integrated Security=True; Connect Timeout=30; Encrypt=False; TrustServerCertificate=False; ApplicationIntent=ReadWrite; MultiSubnetFailover=False;");
 
-
+        //Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
     }
 }
---
+++ CrudRepository/Migrations/20230303182041_InitialMigration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrudRepository.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cadastros",
+                columns: table => new
+                {
+                    ContactID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Mobile = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cadastros", x => x.ContactID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cadastros");
+        }
+    }
+}
